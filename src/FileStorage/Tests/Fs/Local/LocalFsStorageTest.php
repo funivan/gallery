2fb4bb248e535d04bec73c8ba3611b30780f7f62
Ivan Scherbak <dev@funivan.com>
2017-06-11 20:30:48 +0300

    add local fs tests

---
+++ src/FileStorage/Tests/Fs/Local/LocalFsStorageTest.php
@@ -0,0 +1,33 @@
+<?php
+
+  declare(strict_types = 1);
+
+  namespace Funivan\Gallery\FileStorage\Tests\Fs\Local;
+
+  use Funivan\Gallery\FileStorage\Fs\Local\LocalFsStorage;
+  use Funivan\Gallery\FileStorage\Fs\Local\LocalPath;
+  use PHPUnit\Framework\TestCase;
+
+  class LocalFsStorageTest extends TestCase {
+
+    public function testWriteRead() {
+      $fs = new LocalFsStorage(new LocalPath(sys_get_temp_dir()));
+      $fs->write(new LocalPath('test.txt'), 'data-file');
+      self::assertSame(
+        'data-file',
+        $fs->read(new LocalPath('test.txt'))
+      );
+    }
+
+
+    public function testMeta() {
+      $fs = new LocalFsStorage(new LocalPath(sys_get_temp_dir()));
+      $path = new LocalPath('custom-file.json');
+      $fs->write($path, '');
+      self::assertSame(
+        'json',
+        $fs->meta($path, 'extension')
+      );
+    }
+
+  }
