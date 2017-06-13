895ecbc8f0c02225eb39193cd225916ef25b06d1
Ivan Scherbak <dev@funivan.com>
2017-06-13 21:08:25 +0300

    fix tests

--- src/Framework/Dispatcher/Tests/AppTest.php
+++ src/Framework/Dispatcher/Tests/AppTest.php
@@ -4,19 +4,13 @@
   namespace Funivan\Gallery\Framework\Dispatcher\Tests;
 
   use Funivan\Gallery\Framework\Dispatcher\App;
-  use Funivan\Gallery\Framework\Dispatcher\DispatcherInterface;
+  use Funivan\Gallery\Framework\Dispatcher\StaticDispatcher;
   use Funivan\Gallery\Framework\Http\Request\Cookie\RequestCookies;
   use Funivan\Gallery\Framework\Http\Request\Parameters;
   use Funivan\Gallery\Framework\Http\Request\Request;
-  use Funivan\Gallery\Framework\Http\Request\RequestInterface;
-  use Funivan\Gallery\Framework\Http\Response\Body\BodyInterface;
   use Funivan\Gallery\Framework\Http\Response\Headers\Field;
   use Funivan\Gallery\Framework\Http\Response\Headers\Headers;
-  use Funivan\Gallery\Framework\Http\Response\HeadersInterface;
-  use Funivan\Gallery\Framework\Http\Response\Plain\PlainBody;
-  use Funivan\Gallery\Framework\Http\Response\ResponseInterface;
-  use Funivan\Gallery\Framework\Http\Response\Status\ResponseStatus;
-  use Funivan\Gallery\Framework\Http\Response\StatusInterface;
+  use Funivan\Gallery\Framework\Http\Response\Plain\PlainResponse;
   use PHPUnit\Framework\TestCase;
 
   /**
@@ -28,32 +22,20 @@
      * @runInSeparateProcess
      */
     public function testSendResponse(): void {
-      $dispatcher = new class implements DispatcherInterface {
-
-        public function handle(RequestInterface $request): ResponseInterface {
-          return new class implements ResponseInterface {
-
-            public function status(): StatusInterface {
-              return new ResponseStatus(200);
-            }
-
-
-            public function headers(): HeadersInterface {
-              return new Headers([new Field('X-User-Time', '1489')]);
-            }
-
-
-            public function body(): BodyInterface {
-              return new PlainBody('custom body text');
-            }
-          };
-        }
-      };
-
-      $parameters = new Parameters([]);
-      $app = new App($dispatcher);
-      $request = new Request($parameters, $parameters, $parameters, $parameters, RequestCookies::create([]));
-
+      $app = new App(
+        new StaticDispatcher(
+          PlainResponse::createWithHeaders(
+            'custom body text',
+            new Headers([new Field('X-User-Time', '1489')]))
+        )
+      );
+      $request = new Request(
+        new Parameters([]),
+        new Parameters([]),
+        new Parameters([]),
+        new Parameters([]),
+        RequestCookies::create([])
+      );
       ob_start();
       $app->run($request);
       $body = ob_get_clean();
